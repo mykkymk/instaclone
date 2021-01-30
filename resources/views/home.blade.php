b85871788d4bf65456d98556582ed65cf413ae90
Michael Kimani <kimanimyke@gmail.com>
2021-01-30 15:55:51 +0300

    worked on fetching user profile from the database

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,15 +7,18 @@
             <img src="https://instagram.fnbo2-1.fna.fbcdn.net/v/t51.2885-19/s150x150/97566921_2973768799380412_5562195854791540736_n.jpg?_nc_ht=instagram.fnbo2-1.fna.fbcdn.net&_nc_ohc=Qs0j1hhrQ-IAX8NNjHS&tp=1&oh=d0b13b7cd543ccf347539d1459ca7a36&oe=603C0567" alt="" class="rounded-circle">
         </div>
         <div class="col-9 pt-5">
-            <div><h1>freecodecamp</h1></div>
+            <div class="d-flex justify-content-between align-items-baseline">
+                <h1>{{ $user->username }}</h1>
+                <a href="#">Add Post</a>
+            </div>
             <div class="d-flex">
                 <div class="pr-5"><strong>153</strong> posts</div>
                 <div class="pr-5"><strong>23k</strong> followers</div>
                 <div class="pr-5"><strong>212</strong> following</div>
             </div>
-            <div class="pt-4 font-weight-bold">freeCodeCamp.org</div>
-            <div>We're a global community of millions of people learning to code together. We're an open source, donor-supported, 501(c)(3) nonprofit.</div>
-            <div><a href="#">www.freecodecamp.org</a></div>
+            <div class="pt-4 font-weight-bold">{{ $user->profile->title }}</div>
+            <div>{{ $user->profile->description }}</div>
+            <div><a href="#">{{ $user->profile->url }}</a></div>
         </div>
     </div>
 
